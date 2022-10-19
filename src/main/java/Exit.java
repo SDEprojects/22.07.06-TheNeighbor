569de0e111073497625dd012005523b14db668e1
Justin Stowe <prostodevs@gmail.com>
2022-10-19 17:16:46 -0400

    Exit class fixed to new locations JSON attributes

--- src/main/java/Exit.java
+++ src/main/java/Exit.java
@@ -1,60 +1,70 @@
 public class Exit {
 
-    private String name;
-    private String direction;
-    private String description;
-    private String  leadsTo;
+    private String north;
+    private String south;
+    private String east;
+    private String west;
+    private String stairs;
 
     public Exit() {
     }
 
-    public Exit(String name, String direction, String description, String leadsTo) {
-        this.name = name;
-        this.direction = direction;
-        this.description = description;
-        this.leadsTo = leadsTo;
+    public Exit(String north, String south, String east, String west, String stairs) {
+        this.north = north;
+        this.south = south;
+        this.east = east;
+        this.west = west;
+        this.stairs = stairs;
     }
 
-    public String getName() {
-        return name;
+    public String getNorth() {
+        return north;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setNorth(String north) {
+        this.north = north;
     }
 
-    public String getDirection() {
-        return direction;
+    public String getSouth() {
+        return south;
     }
 
-    
-    public void setDirection(String direction) {
-        this.direction = direction;
+    public void setSouth(String south) {
+        this.south = south;
     }
 
-    public String getDescription() {
-        return description;
+    public String getEast() {
+        return east;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setEast(String east) {
+        this.east = east;
     }
 
-    public String getLeadsTo() {
-        return leadsTo;
+    public String getWest() {
+        return west;
     }
 
-    public void setLeadsTo(String leadsTo) {
-        this.leadsTo = leadsTo;
+    public void setWest(String west) {
+        this.west = west;
+    }
+
+    public String getStairs() {
+        return stairs;
+    }
+
+    public void setStairs(String stairs) {
+        this.stairs = stairs;
     }
 
     @Override
     public String toString() {
         return "Exit{" +
-                "name='" + name + '\'' +
-                ", direction='" + direction + '\'' +
-                ", description='" + description + '\'' +
-                ", leadsTo='" + leadsTo + '\'' +
+                "north='" + north + '\'' +
+                ", south='" + south + '\'' +
+                ", east='" + east + '\'' +
+                ", west='" + west + '\'' +
+                ", stairs='" + stairs + '\'' +
                 '}';
     }
 }
