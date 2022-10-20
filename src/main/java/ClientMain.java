29748285a9b54c0c8863b8816be5ebd881796461
Gaurav Banjade <helohsm78@gmail.com>
2022-10-20 15:20:28 -0700

    player exit option

--- src/main/java/ClientMain.java
+++ src/main/java/ClientMain.java
@@ -3,6 +3,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class ClientMain {
     public static void main(String[] args) {
@@ -17,7 +18,7 @@ public class ClientMain {
             List<Items> items = Arrays.asList(mapper.readValue(Paths.get("src/resources/items.json").toFile(), Items[].class));
 
             // convert JSON array to list of locations
-            List<Location> locations = List.of(mapper.readValue(Paths.get("src/resources/locations.json").toFile(),Location[].class));
+            List<Location> locations = List.of(mapper.readValue(Paths.get("src/resources/locations.json").toFile(), Location[].class));
             //Location loc = mapper.readValue(Paths.get("src/resources/locations.json").toFile(),Location.class);
 
 
@@ -29,12 +30,32 @@ public class ClientMain {
             System.out.println("\n" + locations.get(0).getName());
             System.out.println("\n" + locations.get(0).getExit().getNorth());
 
-
-
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+
+        Scanner scanObject = new Scanner(System.in);
+        boolean gameOn = true;
+
+
+        while (gameOn) {
+
+            System.out.println("Welcome to my game" + '\n' + "The game has started");
+            String userCommand = scanObject.nextLine();
+
+            if ( userCommand.equalsIgnoreCase("Quit")) {
+
+                System.out.println("Do you really want to quit the game?");
+                String confirm = scanObject.nextLine();
+                if (confirm.equalsIgnoreCase("Yes")) {
+                    gameOn = false;
+                } else {
+                    continue;
+
+                }
+            }
+
+        }
     }
 
-    }
+}
