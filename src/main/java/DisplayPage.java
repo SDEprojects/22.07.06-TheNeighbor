748c0acef040ebde4894161bbfba9ec34d3a354d
Gaurav Banjade <helohsm78@gmail.com>
2022-10-25 10:32:08 -0700

    player move, AB#260

--- src/main/java/DisplayPage.java
+++ src/main/java/DisplayPage.java
@@ -3,13 +3,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.sql.Array;
+import java.sql.SQLOutput;
+import java.util.*;
 
 
 public class DisplayPage {
-    private final Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     Player player = new Player();
 
     public void execute() {
@@ -46,7 +46,7 @@ public class DisplayPage {
     private void menu() {
 
         System.out.println("\u001B[31m" + ".--------------.\n|" + "\u001B[0m" + " MENU OPTIONS " + "\u001B[31m" +
-                            "|\n'--------------'"+ "\u001B[0m");
+                "|\n'--------------'" + "\u001B[0m");
         System.out.println("Please type your option:\n| INTRO |-------| START GAME |-------| QUIT |\n");
 
         String input;
@@ -89,7 +89,8 @@ public class DisplayPage {
         }
         subMenu();
     }
-    private void quitGame(){
+
+    private void quitGame() {
         try {
             List<String> allLines = Files.readAllLines(Paths.get("src/resources/quitNeighbor.txt"));
             for (String line : allLines) {
@@ -108,14 +109,14 @@ public class DisplayPage {
             input = scanner.nextLine().toLowerCase();
 
             switch (input) {
-                        case "start game":
-                            clearScreen();
-                            startGame();
-                            break;
-                        case "quit":
-                            clearScreen();
-                            quitGame();
-                            System.exit(0);
+                case "start game":
+                    clearScreen();
+                    startGame();
+                    break;
+                case "quit":
+                    clearScreen();
+                    quitGame();
+                    System.exit(0);
                 default:
                     System.out.println("INVALID SELECTION.\n" +
                             "Please type your option:\n" +
@@ -137,33 +138,82 @@ public class DisplayPage {
             // convert JSON array to list of locations
             List<Location> locations = List.of(mapper.readValue(Paths.get("src/resources/locations.json").toFile(), Location[].class));
             //Location loc = mapper.readValue(Paths.get("src/resources/locations.json").toFile(),Location.class);
-            int currentLocationIndex = 0;
             TextParser myTest = null;
+            boolean gameOn = true;
 
-            // user input validation
-            boolean isValid = false;
-            while (!isValid) {
-                System.out.println("Enter an action:\n" +
-                        ">");
-                String test = scanner.next().toLowerCase(); //get user input
-                myTest = new TextParser(test); // pass user input to new TextParser
-                isValid = myTest.getValid(); // set the loop validation to TextParser validation
-                if (!isValid) { // if not valid, will generate invalid message
-                    System.out.println("\nThat is not a valid input. Please try again.\n" +
-                            "Enter 'go', 'look' , or 'take' as a verb");
+            while (gameOn) {
+
+                // user input validation
+                boolean isValid = false;
+                while (!isValid) {
+                    System.out.println("Enter an action:\n" +
+                            ">");
+                    String test = scanner.next().toLowerCase(); //get user input
+                    myTest = new TextParser(test); // pass user input to new TextParser
+                    isValid = myTest.getValid(); // set the loop validation to TextParser validation
+                    if (!isValid) { // if not valid, will generate invalid message
+                        System.out.println("\nThat is not a valid input. Please try again.\n" +
+                                "Enter 'go', 'look' , or 'take' as a verb");
+                    }
+                }
+
+                String noun = "north";
+                String verb = myTest.getVerb();
+
+                int currentLocationIndex = 0;
+
+                while (isValid) {
+                    switch (verb) {
+                        case "go":
+                            String userMap = "";
+                            switch (noun) {
+                                case "north":
+                                    userMap = locations.get(currentLocationIndex).getExit().getNorth();
+                                    break;
+                                case "south":
+                                    userMap = locations.get(currentLocationIndex).getExit().getSouth();
+                                    break;
+                                case "east":
+                                    userMap = locations.get(currentLocationIndex).getExit().getEast();
+                                    break;
+                                case "west":
+                                    userMap = locations.get(currentLocationIndex).getExit().getWest();
+                                    break;
+                                case "stairs":
+                                    userMap = locations.get(currentLocationIndex).getExit().getStairs();
+                                    break;
+                            }
+
+                            System.out.println();
+                            System.out.println("You are in a " + locations.get(currentLocationIndex).getName());
+                            System.out.println(locations.get(currentLocationIndex).getDescription());
+                            Thread.sleep(2000);
+                            System.out.println("Your possible exit routes are");
+                            System.out.println(locations.get(currentLocationIndex).getExit().toString());
+                            Thread.sleep(2000);
+                            System.out.println();
+                            for (int i = 0; i < locations.size(); i++) {
+                                if (locations.get(i).getName().equals(userMap)) {
+                                    currentLocationIndex = i;
+                                }
+                            }
+                            System.out.println("Where would you like to go?");
+                            noun = scanner.nextLine().toLowerCase();
+                            break;
+
+                        case "look":
+                            System.out.println("Looking....");
+                            System.out.println("___________---");
+                            break;
+
+                        case "take":
+                            System.out.println("I am taking something");
+                            System.out.println("**********************");
+                            break;
+                    }
                 }
             }
 
-            String verb = myTest.getVerb();
-            String noun = myTest.getNoun();
-
-
-
-
-
-
-            System.out.printf("the verb is: %s, and the noun is: %s.\n",
-                    verb, noun);
 
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -171,7 +221,8 @@ public class DisplayPage {
         subMenu();
 
     }
-    private void clearScreen(){
+
+    private void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,6 +1,8 @@
 public class Player {
-    public void playerMove(String verb, String noun){
 
+    public void playerMove(String verb, String noun) {
+        System.out.println("Your action is to " + verb);
+        System.out.println("Player is moving to " + noun);
     }
 
 }
